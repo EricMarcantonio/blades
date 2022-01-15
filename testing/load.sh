4b231cf756d9e7ec196e037102ea139c2f803d50
EricMarcantonioIR <90858846+EricMarcantonioIR@users.noreply.github.com>
2022-01-15 16:07:23 -0500

    added some load tests

--- testing/load.sh
+++ testing/load.sh
@@ -1,3 +1,1 @@
 k6 run --vus 100 --duration 1s load.js --summary-export load_100_1s.json
-k6 run --vus 250 --duration 1s load.js --summary-export load_250_1s.json
-k6 run --vus 500 --duration 1s load.js --summary-export load_500_1s.json
